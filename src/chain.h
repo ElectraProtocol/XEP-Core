89d21854068919347cd173be2dfd47a9a8ecaeb5
John Studnicka <ComputerCraftr@users.noreply.github.com>
2020-12-02 05:17:02 -0700

    Decrease future time limit to 15 seconds

--- src/chain.h
+++ src/chain.h
@@ -19,7 +19,7 @@
  * Maximum amount of time that a block timestamp is allowed to exceed the
  * current network-adjusted time before the block will be accepted.
  */
-static constexpr int64_t MAX_FUTURE_BLOCK_TIME = 2 * 60 * 60;
+static constexpr int64_t MAX_FUTURE_BLOCK_TIME = 15; // FTL = 15 seconds (should be <= consensus.nStakeTimestampMask and <= 1/20 of the difficulty averaging window consensus.nPowTargetTimespan)
 
 /**
  * Timestamp window used as a grace period by code that compares external
--- src/timedata.h
+++ src/timedata.h
@@ -10,7 +10,7 @@
 #include <stdint.h>
 #include <vector>
 
-static const int64_t DEFAULT_MAX_TIME_ADJUSTMENT = 70 * 60;
+static const int64_t DEFAULT_MAX_TIME_ADJUSTMENT = 7; // MAX_FUTURE_BLOCK_TIME / 2 = 7.5 seconds
 
 class CNetAddr;
 
