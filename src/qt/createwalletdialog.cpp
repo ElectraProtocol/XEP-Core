463647301004973771a7e9f828dd207ee4648faa
John Studnicka <ComputerCraftr@users.noreply.github.com>
2020-12-29 04:04:29 -0700

    Encrypt wallet by default

--- src/qt/createwalletdialog.cpp
+++ src/qt/createwalletdialog.cpp
@@ -51,6 +51,8 @@ CreateWalletDialog::CreateWalletDialog(QWidget* parent) :
         }
     });
 
+        ui->encrypt_wallet_checkbox->setChecked(true);
+
 #ifndef USE_SQLITE
         ui->descriptor_checkbox->setToolTip(tr("Compiled without sqlite support (required for descriptor wallets)"));
         ui->descriptor_checkbox->setEnabled(false);
