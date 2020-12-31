5e3f08dfd927986077cd762bd81b258cbf9facea
John Studnicka <ComputerCraftr@users.noreply.github.com>
2020-12-31 05:24:24 -0700

    Only request to bech32 and legacy addresses for now

--- src/qt/receivecoinsdialog.cpp
+++ src/qt/receivecoinsdialog.cpp
@@ -151,10 +151,11 @@ void ReceiveCoinsDialog::on_receiveButton_clicked()
     if (ui->useBech32->isChecked()) {
         address_type = OutputType::BECH32;
     } else {
-        address_type = model->wallet().getDefaultAddressType();
+        /*address_type = model->wallet().getDefaultAddressType();
         if (address_type == OutputType::BECH32) {
             address_type = OutputType::P2SH_SEGWIT;
-        }
+        }*/
+        address_type = OutputType::LEGACY; // Revert back to p2sh-segwit when staking is supported
     }
     address = model->getAddressTableModel()->addRow(AddressTableModel::Receive, label, "", address_type);
 
