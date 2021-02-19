22d8023616ba4c280a5ea6aaa2738f4c9fa1cec6
John Studnicka <ComputerCraftr@users.noreply.github.com>
2021-02-19 02:00:46 -0700

    Return invalid state in ConnectBlock for bad PoS blocks

--- src/validation.cpp
+++ src/validation.cpp
@@ -2142,7 +2142,8 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
         return state.Invalid(BlockValidationResult::BLOCK_CONSENSUS, "PoW-ended", strprintf("%s: PoW period ended", __func__));
 
     if (!pindex->GeneratedStakeModifier() && /*pindex->nStakeModifierChecksum == 0 &&*/ !ContextualCheckPoSBlock(block, fProofOfStake, state, view, pindex, chainparams.GetConsensus(), fJustCheck))
-        return error("%s: failed PoS check %s", __func__, state.ToString());
+        return state.Invalid(BlockValidationResult::BLOCK_CONSENSUS, "bad-pos", "proof of stake is incorrect"); // return invalid state here because we don't check in AcceptBlock
+        //return error("%s: failed PoS check %s", __func__, state.ToString());
 
     bool fScriptChecks = true;
     if (!hashAssumeValid.IsNull()) {
